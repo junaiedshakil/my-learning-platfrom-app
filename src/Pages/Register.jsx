b8f0f1827196fb7583c2aa03d267fcb2e26a1030
junaiedshakil <junaiedshakil067@gmail.com>
2025-11-13 13:18:08 +0600

    fixing register page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -3,6 +3,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { AuthContext } from "../Authentication/AuthContext";
 import { updateProfile } from "firebase/auth";
 import { toast } from "react-toastify";
+import { FcGoogle } from "react-icons/fc";
 
 const Register = () => {
   const { createUser, signInWithGoogle, saveUserToDB } =
@@ -127,16 +128,7 @@ const Register = () => {
             onClick={handleGoogleSignup}
             className="flex items-center justify-center gap-2 bg-gradient-to-r from-red-500 to-yellow-400 text-white py-3 rounded-lg font-semibold hover:from-yellow-400 hover:to-red-500 transition-all"
           >
-            <svg
-              className="w-5 h-5 mr-2"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill="currentColor"
-                d="M21.35 11.1h-9.17v2.83h5.3c-.23 1.22-1.49 3.57-5.3 3.57-3.19 0-5.8-2.63-5.8-5.86s2.61-5.86 5.8-5.86c1.82 0 3.04.77 3.73 1.44l2.54-2.48C18.41 3.56 16.01 2.3 12.18 2.3 6.66 2.3 2.3 6.66 2.3 12.18s4.36 9.88 9.88 9.88c5.73 0 9.53-4.03 9.53-9.7 0-.65-.07-1.15-.26-1.26z"
-              />
-            </svg>
+            <FcGoogle />
             Register with Google
           </button>
         </form>
